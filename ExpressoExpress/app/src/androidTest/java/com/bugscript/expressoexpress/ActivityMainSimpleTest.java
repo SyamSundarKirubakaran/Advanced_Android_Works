43e7ba3aa2f22b67e08702645d7f2bf556243d75
Syam Sundar K <k.syamsundar21@gmail.com>
2018-01-12 21:39:25 +0530

    Expresso Express app added

---
+++ ExpressoExpress/app/src/androidTest/java/com/bugscript/expressoexpress/ActivityMainSimpleTest.java
@@ -0,0 +1,35 @@
+package com.bugscript.expressoexpress;
+
+import android.support.test.rule.ActivityTestRule;
+import android.support.test.runner.AndroidJUnit4;
+import android.widget.TextView;
+
+import org.junit.Rule;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+
+import static android.support.test.espresso.Espresso.*;
+import static android.support.test.espresso.action.ViewActions.click;
+import static android.support.test.espresso.assertion.ViewAssertions.matches;
+import static android.support.test.espresso.matcher.ViewMatchers.withId;
+import static android.support.test.espresso.matcher.ViewMatchers.withText;
+
+/**
+ * Created by syamsundark on 12/01/18.
+ */
+
+
+@RunWith(AndroidJUnit4.class)
+public class ActivityMainSimpleTest {
+
+    @Rule
+    public ActivityTestRule<MainActivity> mActivityTestRule =
+            new ActivityTestRule<>(MainActivity.class);
+
+    @Test
+    public void testOnClick(){
+        onView(withId(R.id.imageView)).perform(click());
+        onView(withId(R.id.textView)).check(matches(withText("1")));
+    }
+
+}
---
+++ ExpressoExpress/app/src/main/java/com/bugscript/expressoexpress/MainActivity.java
@@ -0,0 +1,43 @@
+package com.bugscript.expressoexpress;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.widget.ImageView;
+import android.widget.TextView;
+
+import butterknife.BindView;
+import butterknife.ButterKnife;
+import butterknife.OnClick;
+
+public class MainActivity extends AppCompatActivity {
+
+    @BindView(R.id.textView) TextView mNumber;
+    public static int i=0;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        ButterKnife.bind(this);
+    }
+
+    @OnClick(R.id.imageView)
+    public void performActionAdd(){
+        if(i>8){
+
+        }else {
+            i += 1;
+            mNumber.setText(i+"");
+        }
+    }
+
+    @OnClick(R.id.imageView2)
+    public void performActionSub(){
+        if(i<1){
+
+        }else {
+            i -= 1;
+            mNumber.setText(i+"");
+        }
+    }
+}
